0c6b0e4ecdd77946ebf1c8eec812f59bc7b61f8d
daveryderoxford <dave@theryderclan.co.uk>
2025-11-12 04:34:26 +0000

    Removed treasurer switchboard

--- src/app/treasurer/treasurer.routes.ts
+++ src/app/treasurer/treasurer.routes.ts
@@ -1,18 +1,11 @@
 import { Routes } from '@angular/router';
-import { TreasurerSwitchboard } from './treasurer-switchboard';
 import { AccountViewer } from './account-viewer';
 
 export const TREASURER_ROUTES: Routes = [
-   { path: '', redirectTo: 'switchboard', pathMatch: 'full' },
-   {
-      path: 'switchboard',
-      component: TreasurerSwitchboard,
-      title: 'Treasurer switchboard',
-   }, 
+   { path: '', redirectTo: 'accounts', pathMatch: 'full' },
    {
       path: 'accounts',
       component: AccountViewer,
       title: 'Accounts',
-   }, 
-
+   },
 ];
